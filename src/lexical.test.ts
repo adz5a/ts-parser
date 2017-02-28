8497600d69a008997d8d681c424151a4ba508113
antoine zimmermann <antoine.zimmermann@storetail.com>
2017-02-28 12:16:05 +0100

    added test for trim parser

--- src/lexical.test.ts
+++ src/lexical.test.ts
@@ -1,4 +1,6 @@
-import { addop } from "./lexical";
+import { addop, space } from "./lexical";
+import { sat, unit } from "./parser";
+import { many } from "./recursive";
 import { equal, deepEqual } from "assert";
 
 describe("lexical.addop", function () {
@@ -11,5 +13,30 @@ describe("lexical.addop", function () {
         const res = addop("1-2");
         equal(res.length, 1);
         deepEqual(res[0], [-1, ""]);
+    });
+});
+
+describe("lexical.space", function () {
+    it("should parse the leading space", function () {
+        const res = space("   a");
+        equal(res.length, 1);
+        deepEqual(
+            res[0],
+            [ [" ", " ", " "], "a" ]
+        );
+    });
+    describe("lexical.space.trim", function () {
+        const manyLowers = many(sat(c => c >= "a" && c <= "z")); // will parse many lowercase chars
+        const parser = space.bind(
+            _ => manyLowers.bind(
+                tokens => space.bind(_ => unit(tokens))
+            )
+        );
+        const res = parser("  ab  ");
+        equal(res.length, 1);
+        deepEqual(
+            res[0],
+            [ ["a", "b"], "" ]
+        );
     });
 });
--- src/lexical.ts
+++ src/lexical.ts
@@ -1,13 +1,13 @@
-import { Parser, pplus, char, unit, sat } from "./parser";
-import { chainl1 } from "./recursive";
+import { pplus, char, unit, sat } from "./parser";
+import { chainl1, many } from "./recursive";
 
 const plus = char("+");
 const minus = char("-");
 const digit = sat( c => c >= "0" && c <= "9" );
 const addopParser = pplus(
     plus.bind(_ => {
-            return unit((x: number, y: number) => x + y)
-        }),
+        return unit((x: number, y: number) => x + y)
+    }),
     minus.bind(_ => {
         return unit((x: number, y: number) => x - y)
     })
@@ -17,3 +17,5 @@ export const addop = chainl1(
     digit.bind(d => unit(parseInt(d, 10))),
     addopParser
 );
+
+export const space = many(char(" "));
